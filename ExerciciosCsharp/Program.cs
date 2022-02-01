7328ed7cc2a47db5bb46d36cf022ef9c838e1c46
matheuzcapdevila <matheuz.lcc@gmail.com>
2022-01-31 22:07:40 -0300

    Exercicio 1 adicionado

---
+++ ExerciciosCsharp/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Exercicios {
+    class Program {
+        static void Main(string[] args){
+        // Write a C# Sharp program that takes three letters as input and display them in reverse order.
+        
+            // char letter,letter1,letter2;
+    
+            // System.Console.WriteLine("Digite a primeira letra: ");
+            // letter = Convert.ToChar(Console.ReadLine());
+
+            // System.Console.WriteLine("Digite a segunda letra: ");
+            // letter1 = Convert.ToChar(Console.ReadLine());
+
+            // System.Console.WriteLine("Digite a terceira letra: ");
+            // letter2 = Convert.ToChar(Console.ReadLine());
+
+            // System.Console.WriteLine($"De maneira inversa o que foi digitado fica: {letter2}{letter1}{letter}");
+
+            // 2. Write a C# Sharp program that takes a number and a width also a number, as input and then displays a triangle of that width, using that number.
+
+            int numero, Tamanho;
+
+            System.Console.WriteLine("Digite o número a ser repetido");
+            numero = Convert.ToInt32( Console.ReadLine() );
+
+            System.Console.WriteLine("Digite o numero máximo de repetições");
+            Tamanho = Convert.ToInt32(Console.ReadLine());
+
+            
+
+
+    }
+    }
+
+}
